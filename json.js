8d6c1b8ca7e78a337fdceacdfb9d493c02fa6746
Krishna Prasad <kpothers252@gmail.com>
2021-02-09 15:25:28 +0530

    Update json.js

--- json.js
+++ json.js
@@ -1,4 +1,4 @@
-$().ready(function(){
+ready(function(){
     $.getJSON( "https://github.com/krishna1831/kp.github.io/blob/main/data.json", function( data ) {
     console.log(data);
     $("#text").html(data["text"]);
