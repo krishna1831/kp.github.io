8aa07a509f7a60fe09a16c1bbd493c940b96fae8
Krishna Prasad <kpothers252@gmail.com>
2021-02-12 09:08:47 +0530

    Update json.js

--- json.js
+++ json.js
@@ -1,6 +1,1 @@
-ready(function(){
-    $.getJSON( "https://github.com/krishna1831/kp.github.io/blob/main/data.json", function( data ) {
-    console.log(data);
-    $("#text").html(data["text"]);
-  });
-});
+alert(1)
